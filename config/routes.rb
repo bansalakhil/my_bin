e7bd34b229479c53fa0f901276c9835ad1925117
Akhil Bansal <bansalakhil30.10@gmail.com>
2016-05-19 18:39:04 +0530

    Added default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "js_bins#index"
   resources :js_bins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
