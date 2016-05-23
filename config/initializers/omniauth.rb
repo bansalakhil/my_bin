6537662c501c4119bdbdf48e79270502935de98b
Akhil Bansal <bansalakhil30.10@gmail.com>
2016-05-23 19:47:39 +0530

    Added production keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,14 @@
+if Rails.env.development?
+  google_app_client_id = Rails.application.secrets.google_app_client_id
+  google_app_secret = Rails.application.secrets.google_app_secret
+end
+
+if Rails.env.production?
+  google_app_client_id = ENV["GOOGLE_APP_CLIENT_ID"]
+  google_app_secret = ENV["GOOGLE_APP_SECRET"]
+
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.google_app_client_id, Rails.application.secrets.google_app_secret
+  provider :google_oauth2, google_app_client_id, google_app_secret
 end
