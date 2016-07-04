19a6dc766670ea727085b8c1ee54c2218af95cab
Akhil Bansal <bansalakhil30.10@gmail.com>
2016-07-04 13:38:19 +0530

    Added client id for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,14 @@
-if Rails.env.development?
-  google_app_client_id = Rails.application.secrets.google_app_client_id
-  google_app_secret = Rails.application.secrets.google_app_secret
-end
+# if Rails.env.development?
+# end
 
-if Rails.env.production?
-  google_app_client_id = ENV["GOOGLE_APP_CLIENT_ID"]
-  google_app_secret = ENV["GOOGLE_APP_SECRET"]
+# if Rails.env.production?
+#   google_app_client_id = ENV["GOOGLE_APP_CLIENT_ID"]
+#   google_app_secret = ENV["GOOGLE_APP_SECRET"]
 
-end
+# end
+
+google_app_client_id = Rails.application.secrets.google_app_client_id
+google_app_secret = Rails.application.secrets.google_app_secret
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, google_app_client_id, google_app_secret
