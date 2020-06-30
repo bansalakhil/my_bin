74625bab4ceddcc898a041509201eb2ab2d4b858
Akhil Bansal <bansalakhil30.10@gmail.com>
2020-06-30 23:15:14 +0530

    revert back network binding for docker due to server is on old docker

--- app/services/ruby_runner.rb
+++ app/services/ruby_runner.rb
@@ -38,8 +38,9 @@ class RubyRunner
       Rails.logger.debug "Container '#{container_name}' is running.."
     else
       Rails.logger.debug "Container '#{container_name}' is not running... Trying to run"
-      `docker network create #{DOCKER_CONTAINER_NETWORK_NAME}`
-      container_run_cmd  = "run -it --network #{DOCKER_CONTAINER_NETWORK_NAME} --name #{container_name} -d -v /tmp:/tmp bansalakhil/akhil-ruby:2.7.1"
+      # `docker network create #{DOCKER_CONTAINER_NETWORK_NAME}`
+      # container_run_cmd  = "run -it --network #{DOCKER_CONTAINER_NETWORK_NAME} --name #{container_name} -d -v /tmp:/tmp bansalakhil/akhil-ruby:2.7.1"
+      container_run_cmd  = "run -it --name #{container_name} -d -v /tmp:/tmp bansalakhil/akhil-ruby:2.7.1"
       container_stop_cmd = "stop #{container_name}"
       container_rm_cmd   = "rm -f #{container_name}"
 
